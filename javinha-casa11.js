ead010e84a758c7da75e980cb6fc56c250b0087d
Hilary <108356861+Kehilary@users.noreply.github.com>
2022-11-29 12:46:44 -0300

    Update javinha-casa11.js

--- javinha-casa11.js
+++ javinha-casa11.js
@@ -1,98 +1,139 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-canvas.width = 1024;
-canvas.height = 576;
+canvas.width = 1024
+canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravidade = 0.7
 
-class Sprite {
-  constructor({position, velocidade, color = 'red', offset}){
-    this.position = position
-    this.velocidade = velocidade
-    this.height = 150
-    this.width = 50
-    this.lastKey
-    this.ataqueBox ={
-      position: {
-          x: this.position.x,
-          y: this.position.y
-      },
-      offset,
-      width: 100,
-      height: 50,
-    }
-    this.color = color
-    this.atacando 
-    this.health = 100
-  }
-
-
-  draw(){
-    c.fillStyle = this.color
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    if(this.atacando){
-    c.fillStyle = 'green'
-    c.fillRect(this.ataqueBox.position.x, this.ataqueBox.position.y, this.ataqueBox.width, this.ataqueBox.height)
-  }
-}
-
-
-  update(){
-    this.draw()
-    this.ataqueBox.position.x = this.position.x + this.ataqueBox.offset.x
-    this.ataqueBox.position.y = this.position.y
-
-    this.position.x += this.velocidade.x
-    this.position.y += this.velocidade.y
-    if(this.position.y + this.height + this.velocidade.y >= canvas.height){
-        this.velocidade.y = 0
-    }else
-     this.velocidade.y += gravidade
-
-  }
-  attack() {
-    this.atacando = true
-    setTimeout(() =>{
-        this.atacando = false
-    }, 100)
-  }
-  
-}
-
-
-
-
-const player = new Sprite({
+const background = new Sprite({
   position: {
-  x : 200,
-  y : 0
+    x: 0,
+    y: 0
+  },
+  imageSrc: 'imgs-jogo/casaDeAquario.png'
+})
+
+const player = new Fighter({
+  position: {
+    x: 0,
+    y: 0
   },
   velocidade: {
-    x : 0,
-    y : 0
+    x: 0,
+    y: 0
   },
   offset: {
-    x : 0,
-    y : 0
+    x: 0,
+    y: 0
+  },
+  imageSrc: 'imgs-jogo/bronze/HParado.png',
+  framesMax: 4,
+  scale: 2.5,
+  offset: {
+    x: -50,
+    y: 157
+  },
+  sprites: {
+    idle: {
+      imageSrc: 'imgs-jogo/bronze/HParado.png',
+      framesMax: 4
+    },
+    run: {
+      imageSrc: 'imgs-jogo/bronze/HCorre.png',
+      framesMax: 8
+    },
+    jump: {
+      imageSrc: 'imgs-jogo/bronze/HPula.png',
+      framesMax: 2
+    },
+    fall: {
+      imageSrc: 'imgs-jogo/bronze/HCai.png',
+      framesMax: 2
+    },
+    attack1: {
+      imageSrc: 'imgs-jogo/bronze/HAtaque1.png',
+      framesMax: 9
+    },
+    takeHit: {
+      imageSrc: 'imgs-jogo/bronze/HAtacado.png',
+      framesMax: 4
+    },
+    death: {
+      imageSrc: 'imgs-jogo/bronze/HPerde.png',
+      framesMax: 4
+    }
+  },
+  ataqueBox: {
+    offset: {
+      x: 100,
+      y: 50
+    },
+    width: 160,
+    height: 50
   }
 })
 
-const ini = new Sprite({
+const ini = new Fighter({
   position: {
-  x : 700,
-  y : 0
+    x: 400,
+    y: 100
   },
   velocidade: {
-    x : 0,
-    y : 0
+    x: 0,
+    y: 0
   },
   color: 'blue',
   offset: {
-    x : -50,
-    y : 0
+    x: -50,
+    y: 0
+  },
+  imageSrc: 'imgs-jogo/ouro/AParado.png',
+  framesMax: 3,
+  scale: 2.5,
+  offset: {
+    x: -50,
+    y: 167
+  },
+  sprites: {
+    idle: {
+      imageSrc: 'imgs-jogo/ouro/AParado.png',
+      framesMax: 3
+    },
+    run: {
+      imageSrc: 'imgs-jogo/ouro/ACorre.png',
+      framesMax: 8
+    },
+    jump: {
+      imageSrc: 'imgs-jogo/ouro/APula.png',
+      framesMax: 2
+    },
+    fall: {
+      imageSrc: 'imgs-jogo/ouro/ACai.png',
+      framesMax: 2
+    },
+    attack1: {
+      imageSrc: 'imgs-jogo/ouro/AAtaque1.png',
+      framesMax: 10
+    },
+    takeHit: {
+      imageSrc: 'imgs-jogo/ouro/AAtacado.png',
+      framesMax: 3
+    },
+    death: {
+      imageSrc: 'imgs-jogo/ouro/APerde.png',
+      framesMax: 2
+    }
+  },
+  ataqueBox: {
+    offset: {
+      x: -170,
+      y: 50
+    },
+    width: 170,
+    height: 50
   }
 })
 
@@ -105,54 +146,22 @@ const keys = {
   d: {
     pressed: false
   },
-  w: {
-    pressed: false 
-  },
   ArrowRight: {
     pressed: false
   },
   ArrowLeft: {
     pressed: false
-  },
-  ArrowUp: {
-    pressed: false
-  },
- 
-}
- 
-function retanguloColisao({retan1,retan2}){
-      return(retan1.ataqueBox.position.x + retan1.ataqueBox.width >= retan2.position.x && retan1.ataqueBox.position.x <= retan2.position.x + retan2.width &&
-        retan1.ataqueBox.position.y + retan1.ataqueBox.height >= retan2.position.y && retan1.ataqueBox.position.y <= retan2.position.y + retan2.height)
+  }
 }
 
-function determinarVencedor({player, ini, timerId}){
-  clearTimeout(timerId)
-  document.querySelector('#resultado').style.display = 'flex'
-  if(player.health > ini.health){
-    document.querySelector('#resultado').innerHTML = 'Vencedor: Cavaleiro de Bronze'
-  }   
-  else  if(player.health < ini.health){
-    document.querySelector('#resultado').innerHTML = 'Vencedor: Cavaleiro de Ouro'
-  }   
-}
- 
-let timer = 100
-let timerId 
-function decreaseTimer(){
-  if(timer > 0){
-   timerId = setTimeout(decreaseTimer, 1000)
-    timer--
-    document.querySelector('#tempo').innerHTML = timer
-  }
-  if(timer === 0){
-    determinarVencedor({player, ini, timerId})
-  }
-}
 decreaseTimer()
 
-function anima(){
+function anima() {
   window.requestAnimationFrame(anima)
   c.fillStyle = 'black'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  background.update()
+  c.fillStyle = 'rgba(255, 255, 255, 0.15)'
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.update()
   ini.update()
@@ -160,90 +169,149 @@ function anima(){
   player.velocidade.x = 0
   ini.velocidade.x = 0
 
-  if(keys.a.pressed && player.lastKey === 'a'){
-    player.velocidade.x = -8
-  }
-  else if(keys.d.pressed  && player.lastKey === 'd'){
-    player.velocidade.x = 8
-  }
-  if(keys.ArrowLeft.pressed && ini.lastKey === 'ArrowLeft'){
-    ini.velocidade.x = -8
-  }
-  else if(keys.ArrowRight.pressed  && ini.lastKey === 'ArrowRight'){
-    ini.velocidade.x = 8
+  // bronze - movimento
+  if (keys.a.pressed && player.lastKey === 'a') {
+    player.velocidade.x = -5
+    player.switchSprite('run')
+  } else if (keys.d.pressed && player.lastKey === 'd') {
+    player.velocidade.x = 5
+    player.switchSprite('run')
+  } else {
+    player.switchSprite('idle')
   }
 
-  if(retanguloColisao({retan1:player, retan2:ini}) && player.atacando){
+  // pulando
+  if (player.velocidade.y < 0) {
+    player.switchSprite('jump')
+  } else if (player.velocidade.y > 0) {
+    player.switchSprite('fall')
+  }
+
+  // ouro - movimento
+  if (keys.ArrowLeft.pressed && ini.lastKey === 'ArrowLeft') {
+    ini.velocidade.x = -5
+    ini.switchSprite('run')
+  } else if (keys.ArrowRight.pressed && ini.lastKey === 'ArrowRight') {
+    ini.velocidade.x = 5
+    ini.switchSprite('run')
+  } else {
+    ini.switchSprite('idle')
+  }
+
+  // pulando
+  if (ini.velocidade.y < 0) {
+    ini.switchSprite('jump')
+  } else if (ini.velocidade.y > 0) {
+    ini.switchSprite('fall')
+  }
+
+  // detecta a colisão e o inimigo é atingido
+  if (
+    retanguloColisao({
+      retan1: player,
+      retan2: ini
+    }) &&
+    player.atacando &&
+    player.framesCurrent === 4
+  ) {
+    ini.takeHit()
     player.atacando = false
-    ini.health -= 20
-    document.querySelector('#vida-ini').style.width = ini.health + '%'
+
+    gsap.to('#vida-ini', {
+      width: ini.health + '%'
+    })
   }
 
-  if(retanguloColisao({retan1:ini, retan2:player}) && ini.atacando){
+  // se o cav. de bronze errar
+  if (player.atacando && player.framesCurrent === 4) {
+    player.atacando = false
+  }
+
+  // this is where our player gets hit
+  if (
+    retanguloColisao({
+      retan1: ini,
+      retan2: player
+    }) &&
+    ini.atacando &&
+    ini.framesCurrent === 2
+  ) {
+    player.takeHit()
     ini.atacando = false
-    player.health -= 20
-    document.querySelector('#vida-player').style.width = player.health + '%'
 
+    gsap.to('#vida-player', {
+      width: player.health + '%'
+    })
   }
-  if(ini.health <= 0 || player.health <= 0){
-    determinarVencedor({player, ini, timerId})
+
+  // se o cav. de ouro errar
+  if (ini.atacando && ini.framesCurrent === 2) {
+    ini.atacando = false
+  }
+
+  // fim de jogo com base na vida
+  if (ini.health <= 0 || player.health <= 0) {
+    determinarVencedor({ player, ini, timerId })
   }
 }
 anima()
 
-window.addEventListener('keydown', (event) =>{
-      switch (event.key){
-        case 'd':
-          keys.d.pressed = true
-          player.lastKey = 'd'
-          break
-        case 'a':
-          keys.a.pressed = true
-          player.lastKey = 'a'
-          break
-        case 'w':
-          player.velocidade.y = -20
-            break
-        case ' ':
+window.addEventListener('keydown', (event) => {
+  if (!player.dead) {
+    switch (event.key) {
+      case 'd':
+        keys.d.pressed = true
+        player.lastKey = 'd'
+        break
+      case 'a':
+        keys.a.pressed = true
+        player.lastKey = 'a'
+        break
+      case 'w':
+        player.velocidade.y = -20
+        break
+      case 's':
         player.attack()
         break
-       case 'ArrowRight':
-          keys.ArrowRight.pressed = true
-          ini.lastKey = 'ArrowRight'
-            break
-        case 'ArrowLeft':
-          keys.ArrowLeft.pressed = true
-            ini.lastKey = 'ArrowLeft'
-              break
-         case 'ArrowUp':
-          ini.velocidade.y = -20
-            break
-          case 'ArrowDown':
-            ini.atacando = true
-                break
-        }
+    }
+  }
+
+  if (!ini.dead) {
+    switch (event.key) {
+      case 'ArrowRight':
+        keys.ArrowRight.pressed = true
+        ini.lastKey = 'ArrowRight'
+        break
+      case 'ArrowLeft':
+        keys.ArrowLeft.pressed = true
+        ini.lastKey = 'ArrowLeft'
+        break
+      case 'ArrowUp':
+        ini.velocidade.y = -20
+        break
+      case 'ArrowDown':
+        ini.attack()
+        break
+    }
+  }
 })
-window.addEventListener('keyup', (event) =>{
-  switch (event.key){
-  case 'd':
-    keys.d.pressed = false
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false
       break
-  case 'a':
-    keys.a.pressed = false
+    case 'a':
+      keys.a.pressed = false
       break
-  case 'w':
-    keys.w.pressed = false
+  }
+
+  switch (event.key) {
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = false
       break
- }
-switch (event.key){
-  case 'ArrowRight':
-    keys.ArrowRight.pressed = false
-      break
-  case 'ArrowLeft':
-    keys.ArrowLeft.pressed = false
-      break
-  case 'ArrowUp':
-    keys.ArrowUp.pressed = false
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = false
       break
   }
 })
